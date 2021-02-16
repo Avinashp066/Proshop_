09183fac0f6394fa1ba3a5cf0e811b399de3a03e
Avinashp066 <Avinashp066@gmail.com>
2021-02-16 20:54:15 +0530

    Hashed password

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
-const express = require('express');
-// const dotenv = require('dotenv');
-const products = require ('./data/products');
+import express from 'express';
+import dotenv  from 'dotenv';
+import colors  from 'colors';
+import connectDB from '../backend/config/db.js';
+import products  from './data/products.js';
 
-// dotenv.config()
+dotenv.config()
+
+connectDB();
 
 const app =express();
 
@@ -23,4 +27,4 @@ app.get('/api/products/:id', (req, res) => {
 
 const PORT =process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`));
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`.underline.yellow));
